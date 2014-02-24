87b3cc4e08b893262f17291b71434e388acfc65c
Colin Craig <cacraig@ncsu.edu>
2014-02-23 19:51:01 -0800

    added batch image gen file

---
+++ data_getters/batchGenerateImages.py
@@ -0,0 +1,13 @@
+import os
+
+class BatchGenerateImages:
+  def __init__(self, dataGetter):
+    self.dataGetter = dataGetter
+    return
+
+  def getCommand(self):
+    cmd = "perl scripts/gempak.pl "
+    for key,http in self.dataGetter.constants.modelGems.items():
+      fileArgString = key + "=" + http['file']
+      cmd += fileArgString + " "
+    return cmd
