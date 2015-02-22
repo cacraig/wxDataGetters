cb9374bd886032e89edf3346806488988b008771
Colin Craig <cacraig@ncsu.edu>
2015-02-22 12:47:59 -0800

    Fixed yet another bug

--- data_getters/dataChecker.py
+++ data_getters/dataChecker.py
@@ -23,7 +23,7 @@ def main():
   beanstalkdConn = beanstalkc.Connection(host=constants.beanstalkdHost, port=11300)
 
   resetHourKeys(constants, model, redisConn)
-  redisConn.set(model + '-complete', constants.runTimes[model])
+  redisConn.set(model + '-complete', db.getCurrentRun())
 
   while True:
     constants = Constants(model)
