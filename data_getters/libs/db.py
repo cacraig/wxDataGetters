7cafd24b5135312de8f547e3103bdacaa6df5018
Colin Craig <cacraig@ncsu.edu>
2015-02-22 12:46:46 -0800

    Fixed yet another bug

--- data_getters/libs/db.py
+++ data_getters/libs/db.py
@@ -8,14 +8,18 @@ class NgwipsDB:
     return
 
   def isNewRun(self, model, currentRun):
-    self.cursor.execute("SELECT current_run from model where name='" + model + "'")
-    rows = self.cursor.fetchall()
-
+    currentDbRun = self.getCurrentRun(model)
     # If it is a new run... return True.
-    if int(rows[0][0]) < int(currentRun):
+    if int(currentDbRun) < int(currentRun):
       return True
     else:
       return False
+
+  def getCurrentRun(self, model):
+    self.cursor.execute("SELECT current_run from model where name='" + model + "'")
+    rows = self.cursor.fetchall()
+    currentRun = rows[0][0]
+    return currentRun
 
   def updateModelRun(self):
     return
